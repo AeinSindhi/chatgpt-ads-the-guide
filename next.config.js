375980b61fd6f2136a11d96e6ad5f7828e562f6f
Aein Sindhi <100613269+AeinSindhi@users.noreply.github.com>
2026-09-06 14:43:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: "export",
-  basePath: "/chatgpt-ads-the-guide",
-  assetPrefix: "/chatgpt-ads-the-guide/",
   trailingSlash: true,
   images: {
     unoptimized: true,
